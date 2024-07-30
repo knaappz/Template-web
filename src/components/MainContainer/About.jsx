95a05a68a6e25bd7138e38ad4f1aac3ec2e05b78
Jakub Knapik <128633057+Knaappz@users.noreply.github.com>
2024-07-30 21:29:36 +0200

    gradient edit

--- src/components/MainContainer/About.jsx
+++ src/components/MainContainer/About.jsx
@@ -15,9 +15,9 @@ export default function About() {
                 Tempora animi, ut labore tempore pariatur quos hic sit aliquid quibusdam. Qui, odit id!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, natus. Aliquam autem facilis et dolorum hic. 
                 Tempora animi, ut labore tempore pariatur quos hic sit aliquid quibusdam. Qui, odit id!
             </p>
+            <img className='profile-img' src="src/assets/user.png" alt="//error-img//" />
 
           </div>
-          <img className='profile-img' src="src/assets/user.png" alt="//error-img//" />
 
         </div>
     </section> 
