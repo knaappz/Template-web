9020370a4cc16e44769a3f0f9be9d01fa9acd433
Jakub <knapikj.priv@gmail.com>
2024-08-05 13:54:05 +0200

    Add responibility for maincontainer

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,19 +10,24 @@ export default function Portfolio() {
           <h2 className="section-title">Moje projekty</h2>
 
                 <section className='box-row'>
-                    <div to='/helsinki' className="box" onClick={handleHelsinki}>
-                        <img src="../../../assets/helsinki/model1.jpg" alt="" />
-                        <h3>Helsinki</h3>
+                    <div className="box-pair">
+                        <div to='/helsinki' className="box" onClick={handleHelsinki}>
+                            <img src="../../../assets/helsinki/model1.jpg" alt="" />
+                            <h3>Helsinki</h3>
+                        </div>
+                        <div className="box" onClick={handleZiola}>
+                            <h3>Ziola</h3>
+                        </div>
                     </div>
-                    <div className="box" onClick={handleZiola}>
-                        <h3>Ziola</h3>
-                    </div>
-                    <div className="box">
-                        <h3>Nazwa projektu 3</h3>
-                    
-                    </div>
-                    <div className="box">
-                        <h3>Nazwa projektu 4</h3>
+
+                    <div className="box-pair">
+                        <div className="box">
+                            <h3>Nazwa projektu 3</h3>
+                        
+                        </div>
+                        <div className="box">
+                            <h3>Nazwa projektu 4</h3>
+                        </div>
                     </div>
                 </section>
           </div>
